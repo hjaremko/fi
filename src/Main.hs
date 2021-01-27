811a4fd4c25c92dbf7f1dc30426b50b467815f55
Hubert Jaremko <hjaremko@outlook.com>
2021-01-27 23:08:20 +0100

    Clean up main

--- src/Main.hs
+++ src/Main.hs
@@ -23,15 +23,12 @@ parse s = map fst (parseHelp s [])
 data Var = Var String Float deriving Show
 type State = [Var]
 type JumpData =  [(Label, Int)]
-data Context = Ctx State JumpData [Statement]
-
--- toJumpData (i, Loop _ _ _ stmts) = readJumpData stmts
+type Context = (State, JumpData, [Statement])
 
 concatInnerStatements :: [Statement] -> [Statement]
 concatInnerStatements [] = []
 concatInnerStatements (Loop s _ _ ss:xs) = s:concatInnerStatements ss ++ concatInnerStatements xs
 concatInnerStatements (x:xs) = x:concatInnerStatements xs
-
 
 readJumpData ::[Statement] -> JumpData
 readJumpData sts = filter (\(_,i) -> i /= -1) (concatMap toJumpData (addIndex sts))
@@ -49,17 +46,10 @@ isInitialized :: [Var] -> String -> Bool
 isInitialized [] _ = False;
 isInitialized (Var id val:vs) name = (id == name) || isInitialized vs name
 
--- evalExpr :: Expr -> Float
--- evalExpr (FloatLiteral val) = val
-
 evalExpr :: Expr -> State -> Float
 evalExpr (FloatLiteral x) _ = x
 evalExpr (VarId i) state = varValue i state
 evalExpr (Arithm tokens) state = evalRpn (toRpn tokens [] []) [] state
-
--- evalArithmetic :: [Token] -> State -> Float
--- evalArithmetic tokens state = evalRpn (toRpn tokens [] []) [] state
-
 
 evalRpn :: [Token] -> [Float] -> State -> Float
 evalRpn [] [s] _ = s
@@ -71,7 +61,6 @@ evalRpn (Mult:ops) (a:b:stack) state = evalRpn ops ((b * a):stack) state
 evalRpn (Div:ops) (a:b:stack) state = evalRpn ops ((b / a):stack) state
 evalRpn (Sqrt:ops) (a:stack) state = evalRpn ops (sqrt a:stack) state
 
-
 evalAssignment :: State -> String -> Float -> State
 evalAssignment vars name val
     = if isInitialized vars name then
@@ -79,79 +68,67 @@ evalAssignment vars name val
         else
             Var name val:vars
 
-
 varValue :: String -> State -> Float
 varValue name vars = findVarValue vars name
 
 readSt :: State -> String -> IO State
-readSt state id = do 
-    n <- getLine
-    return (evalAssignment state id (read n))
+readSt state id = evalAssignment state id . read <$> getLine
 
-evalOne :: Statement -> State -> JumpData -> [Statement] -> IO State
-evalOne (Assignment id expr) state jd all
+evalOne :: Statement -> Context -> IO State
+evalOne (Assignment id expr) (state, jd, all)
     = return (evalAssignment state id (evalExpr expr state))
     
-evalOne (Print []) state jd all = do
+evalOne (Print []) (state,_,_) = do
     putStr "\n"
     return state
 
-evalOne (Print (Expr e:xs)) state jd all = do
+evalOne (Print (Expr e:xs)) (state, jd, all) = do
     putStr $ show $ evalExpr e state
-    evalOne (Print xs) state jd all
+    evalOne (Print xs) (state, jd, all)
     
--- evalOne (Print (PVar id:xs)) state jd all = do
---     putStr $ show $ varValue id state
---     evalOne (Print xs) state jd all
-
-evalOne (Print (Str s:xs)) state jd all = do
+evalOne (Print (Str s:xs)) ctx = do
     putStr s
-    evalOne (Print xs) state jd all
+    evalOne (Print xs) ctx
 
-evalOne (Read id) state jd all = do
-    n <- getLine
-    return (evalAssignment state id (read n))
+evalOne (Read id) (state, jd, all) =
+     evalAssignment state id .read  <$> getLine
     
-evalOne (Loop (LabelStmt _ assign) stop step stmts) state jd all =
-    evalOne (Loop assign stop step stmts) state jd all
+evalOne (Loop (LabelStmt _ assign) stop step stmts) ctx =
+    evalOne (Loop assign stop step stmts) ctx
 
-evalOne (Loop (Assignment id assignExpr) stopExpr stepExpr stmts) state jd all = do
+evalOne (Loop (Assignment id assignExpr) stopExpr stepExpr stmts) (state, jd, all) = do
     let val = evalExpr assignExpr state
     let stop = evalExpr stopExpr state
     let step = evalExpr stepExpr state
-    s <- evalOne (Assignment id (FloatLiteral val)) state jd all
+    s <- evalOne (Assignment id (FloatLiteral val)) (state, jd, all)
     if val < stop then do
-        s' <- eval stmts s jd all
+        s' <- eval stmts (s, jd, all)
         let nextLoop = Loop (Assignment id (FloatLiteral (val + step))) (FloatLiteral stop) (FloatLiteral step) stmts
-        evalOne nextLoop s' jd all
+        evalOne nextLoop (s', jd, all)
     else return s
         
-evalOne (LabelStmt _ stmt) state jd all = evalOne stmt state jd all
-
--- evalOne (LabelStmt _ stmt) state jd all = evalOne stmt state jd all
+evalOne (LabelStmt _ stmt) ctx = evalOne stmt ctx
 
 evalGoto :: Label -> JumpData -> [Statement] -> [Statement]
-evalGoto label jd all = drop (getIdx -1) all
-    where getIdx = head $ map snd $ filter (\(l,i)-> l == label) jd
+evalGoto label jd = drop (getIdx -1)
+    where getIdx = head . map snd $ filter (\(l,i)-> l == label) jd
     
-eval :: [Statement] -> State -> JumpData -> [Statement] -> IO State
-eval [] s _ _ = return s
+eval :: [Statement] -> Context -> IO State
+eval [] (s,_,_) = return s
 
-eval (End: _) s _ _ = return s
+eval (End: _) (s,_,_) = return s
 
-eval (Goto label:_) state jd all = do
-    s <- eval (evalGoto label jd all)  state jd all
-    return s
+eval (Goto label:_) (state, jd, all) =
+    eval (evalGoto label jd all)  (state, jd, all)
 
-eval (If expr neg zero pos:_) state jd all 
- | evalExpr expr state < 0 = eval (evalGoto neg jd all) state jd all
- | evalExpr expr state == 0 = eval (evalGoto zero jd all)  state jd all
- | evalExpr expr state > 0 =    eval (evalGoto pos jd all) state jd all
+eval (If expr neg zero pos:_) (state, jd, all) 
+ | evalExpr expr state < 0 = eval (evalGoto neg jd all) (state, jd, all)
+ | evalExpr expr state == 0 = eval (evalGoto zero jd all)  (state, jd, all)
+ | evalExpr expr state > 0 =    eval (evalGoto pos jd all) (state, jd, all)
 
-eval (statement:rest) state jd all = do
-    -- print rest
-    r <- evalOne statement state jd all
-    eval rest r jd all
+eval (statement:rest) (state, jd, all) = do
+    r <- evalOne statement (state, jd, all)
+    eval rest (r, jd, all)
 
 main :: IO ()
 main = do
@@ -159,13 +136,9 @@ main = do
   fileContent <- readFile filename
 
   let parsed = parse fileContent
-  print parsed
-
   let allStatements = concatInnerStatements parsed
   let jd = readJumpData allStatements
-  print jd
---   print allStatements
+  let ctx = ([], jd, allStatements)
   
-  eval parsed [] jd allStatements
---   print parsed
+  eval parsed ctx
   return ()
