1437fdcdeb482742567b231fe47d5626e43c4f48
Hubert Jaremko <hjaremko@outlook.com>
2021-01-30 17:54:29 +0100

    Add do loops with step

--- src/Parse/Statement.hs
+++ src/Parse/Statement.hs
@@ -5,56 +5,56 @@ module Parse.Statement where
 
 import Grammar.Grammar
 import Parse.Assignment
+import Parse.Expression
+import Parse.Goto
 import Parse.If
 import Parse.Io
 import Parse.Primitive
-import Parse.Expression
-import Parse.Goto
 
-continue :: Parser Statement 
-continue = 
-              string "CONTINUE" `bind` \_ ->
-                result Continue
+continue :: Parser Statement
+continue =
+  string "CONTINUE" `bind` \_ ->
+    result Continue
 
-do' :: Parser Statement 
-do' = string "DO" `bind` \_ ->
-      spaces `bind` \_ -> 
+do' :: Parser Statement
+do' =
+  string "DO" `bind` \_ ->
+    spaces `bind` \_ ->
       manyNotEmpty digit `bind` \label ->
-      spaces `bind` \_ -> 
-      assignment `bind` \start ->
-      char ',' `bind` \_ ->
-      spaces `bind` \_ -> 
-      expression `bind` \stop ->
-      spaces `bind` \s -> 
-      result (LabelStmt (read label) (Loop start stop (FloatLiteral 1)))
-      
--- stepDo' :: Parser Statement 
--- stepDo' = string "DO" `bind` \x ->
---       spaces `bind` \y -> 
---       assignment `plus` labelAssignment  `bind` \start ->
---       char ',' `bind` \q ->
---       spaces `bind` \w -> 
---       expression `bind` \stop ->
---       char ',' `bind` \q ->
---       spaces `bind` \s -> 
---       expression `bind` \step ->
---       many statement `bind` \stmts ->
---       spaces `bind` \s -> 
---       string "END DO" `bind` \xs ->
---       result (Loop start stop step (stmts ++ [End]))
+        spaces `bind` \_ ->
+          assignment `bind` \start ->
+            char ',' `bind` \_ ->
+              spaces `bind` \_ ->
+                expression `bind` \stop ->
+                  spaces `bind` \_ ->
+                    result (LabelStmt (read label) (Loop start stop (FloatLiteral 1)))
+
+stepDo' :: Parser Statement
+stepDo' =
+  string "DO" `bind` \_ ->
+    spaces `bind` \_ ->
+      manyNotEmpty digit `bind` \label ->
+        spaces `bind` \_ ->
+          assignment `bind` \start ->
+            char ',' `bind` \_ ->
+              spaces `bind` \_ ->
+                expression `bind` \stop ->
+                  spaces `bind` \_ ->
+                    char ',' `bind` \_ ->
+                      spaces `bind` \_ ->
+                        expression `bind` \step ->
+                          result (LabelStmt (read label) (Loop start stop step))
 
 doLoop :: Parser Statement
-doLoop = first $ consumeLeadingSpaces $ do' --`plus` stepDo' 
-
+doLoop = first $ consumeLeadingSpaces $ stepDo' `plus` do'
 
 labelAssignment :: Parser Statement
 labelAssignment =
-      manyNotEmpty digit
-      `bind` \x ->
-        spaces `bind` \s ->
-          assignment `bind` \st ->
-            result (LabelStmt (read x) st)
-
+  manyNotEmpty digit
+    `bind` \x ->
+      spaces `bind` \s ->
+        assignment `bind` \st ->
+          result (LabelStmt (read x) st)
 
 labelStatement :: Parser Statement
 labelStatement =
